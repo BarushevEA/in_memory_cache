c3c09ee092791015a3c58deae417e24ad4242e8a
Evgeny <b_evgeny@ukr.net>
2025-05-20 17:49:05 +0300

    Add factory functions for concurrent and sharded caches
    Introduced `NewConcurrentCache` and `NewShardedCache` functions to simplify the creation of in-memory caches with TTL support. These abstractions make it easier to initialize specific cache implementations with consistent parameters.

--- pkg/service.go
+++ pkg/service.go
@@ -1,1 +1,15 @@
 package pkg
+
+import (
+	"context"
+	"github.com/BarushevEA/in_memory_cache/internal/src"
+	"time"
+)
+
+func NewConcurrentCache[T any](ctx context.Context, ttl, ttlDecrement time.Duration) ICache[T] {
+	return src.NewConcurrentMapWithTTL[T](ctx, ttl, ttlDecrement)
+}
+
+func NewShardedCache[T any](ctx context.Context, ttl, ttlDecrement time.Duration) ICache[T] {
+	return src.NewDynamicShardedMapWithTTL[T](ctx, ttl, ttlDecrement)
+}
