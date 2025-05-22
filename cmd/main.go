105580fc1012515525b7b44d1fb39bbcac051f8e
Evgeny <b_evgeny@ukr.net>
2025-05-22 15:38:04 +0300

    Refactor cache benchmarking logic in main.go
    Introduce a "bench" helper function to generalize benchmarking for different cache implementations. Enhance output to include cache type and benchmark duration for better clarity during performance analysis.

--- cmd/main.go
+++ cmd/main.go
@@ -7,19 +7,24 @@ import (
 	"time"
 )
 
+// Test represents a simple structure with name and age fields.
 type Test struct {
 	name string
 	age  int
 }
 
+// main acts as the program's entry point, benchmarking different cache implementations with specified TTL configurations.
 func main() {
 	ctx := context.Background()
 	ttl := 1 * time.Second
 	ttlDecrement := 500 * time.Millisecond
 
-	cache := pkg.NewShardedCache[*Test](ctx, ttl, ttlDecrement)
-	//cache := pkg.NewConcurrentCache[*Test](ctx, ttl, ttlDecrement)
+	bench(pkg.NewShardedCache[*Test](ctx, ttl, ttlDecrement), "ShardedCache")
+	bench(pkg.NewConcurrentCache[*Test](ctx, ttl, ttlDecrement), "ConcurrentCache")
+}
 
+// bench measures and prints the duration of setting 10 million entries in a cache implementing the ICache interface.
+func bench(cache pkg.ICache[*Test], cacheName string) {
 	start := time.Now()
 	for i := 0; i < 10000000; i++ {
 		err := cache.Set(fmt.Sprintf("%dkey", i), &Test{"name", 20})
@@ -29,5 +34,5 @@ func main() {
 		}
 	}
 
-	fmt.Println(time.Since(start), cache.Len())
+	fmt.Println(cacheName, "Duration:", time.Since(start), "cache.Len:", cache.Len())
 }
